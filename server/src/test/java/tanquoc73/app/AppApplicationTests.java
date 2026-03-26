d73342de71539274ffecced72630a126987ed140
TanQuoc-73 <cutorasusu2004@Gmail.com>
2026-03-26 09:25:42 +0700

    ini_prj

---
+++ server/src/test/java/tanquoc73/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package tanquoc73.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
